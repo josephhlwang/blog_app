d3b26487e8eecc173f06e75d6a9304dd32f90f40
josephhlwang <joseph001126@gmail.com>
2022-05-06 05:00:27 -0400

    Added recent/related posts widget.

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import Link from 'next/link'
 
-const PostWidget = () => {
+import { getRecentPosts, getSimilarPosts } from '../services'
+
+const PostWidget = ({ categories, slug }: any) => {
+  const [relatedPosts, setRelatedPosts] = useState([])
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      )
+    } else {
+      getRecentPosts().then((result) => setRelatedPosts(result))
+    }
+  }, [slug])
+
+  console.log(relatedPosts)
+
   return (
-    <div>PostWidget</div>
+    <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        {slug ? 'Related Posts' : 'Recent Posts'}
+      </h3>
+      {relatedPosts.map((post: any, index: any) => (
+        <div key={index} className="mb-4 flex w-full items-center">
+          <div className="w-16 flex-none">
+            <img
+              alt={post.title}
+              className="h-14 w-14 rounded-full align-middle"
+              src={post.featuredImage.url}
+            />
+          </div>
+          <div className="ml-4 flex-grow">
+            <p className="font-xs text-gray-500">
+              {moment(post.createdAt).format('MMM DD, YYYY')}
+            </p>
+            <Link href={`/post/${post.slug}`} key={post.title}>
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
--- services/index.js
+++ services/index.js
@@ -4,7 +4,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 
 export const getPosts = async () => {
   const query = gql`
-    query getPosts {
+    query getPosts() {
       postsConnection {
         edges {
           node {
@@ -40,13 +40,39 @@ export const getPosts = async () => {
 
 export const getRecentPosts = async () => {
   const query = gql`
-    query GetRecentPosts() {
+    query GetPostDetails() {
       posts(
-        orderBy: createAt_ASC
+        orderBy: createdAt_ASC
         last: 3
       ){
         title
         featuredImage{
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const results = await request(graphqlAPI, query)
+
+  return results.posts
+}
+
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails(${slug}: String!, ${categories}: [String!]) {
+      posts(
+        where: {
+          slug_not: ${slug}
+          AND: { categories_some: { slug_in: ${categories} } }
+        }
+        orderBy: createAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
           url
         }
         createdAt
