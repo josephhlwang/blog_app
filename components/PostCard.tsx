0f05a7af5a19e3f4e014a9192b34fafabd59c3d8
josephhlwang <joseph001126@gmail.com>
2022-05-06 00:13:14 -0400

    Added GraphQL API fetching for posts.

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import moment from 'moment'
+import Link from 'next/link'
 
 const PostCard = ({ post }: any) => {
+  console.log(post)
   return (
-    <div>
-      {post.title}
-      {post.excerpt}
+    <div className="mb-8 rounded-lg bg-white p-0 pb-12 shadow-lg lg:p-8">
+      <div className="relative mb-6 overflow-hidden pb-80 shadow-md">
+        <img
+          src={post.featuredImage.url}
+          alt={post.title}
+          className="absolute h-80 w-full rounded-t-lg object-cover object-top shadow-lg lg:rounded-lg"
+        />
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+import { getPosts } from '../services'
+
 import { PostCard, Categories, PostWidget } from '../components'
 
-const posts = [
-  { title: 'React Testing', excerpt: 'Learn React Testing' },
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
-]
-
-const Home: NextPage = () => {
+const Home: NextPage = ({ posts }: any) => {
   return (
     <div className="container mx-auto mb-8 px-10">
       <Head>
@@ -18,8 +15,8 @@ const Home: NextPage = () => {
       </Head>
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post, index) => (
-            <PostCard post={post} key={post.title} />
+          {posts.map((post: any) => (
+            <PostCard post={post.node} key={post.node.title} />
           ))}
         </div>
         <div className="col-span-1 lg:col-span-4">
@@ -31,6 +28,14 @@ const Home: NextPage = () => {
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+
+  return {
+    props: { posts },
+  }
 }
 
 export default Home
