757ee26db7087a6f92d43517065c7e93c121c16f
josephhlwang <joseph001126@gmail.com>
2022-05-25 19:17:16 -0400

    Updated function names and added getPostDetails function.

--- services/index.js
+++ services/index.js
@@ -40,7 +40,7 @@ export const getPosts = async () => {
 
 export const getRecentPosts = async () => {
   const query = gql`
-    query GetPostDetails() {
+    query getRecentPosts() {
       posts(
         orderBy: createdAt_ASC
         last: 3
@@ -62,13 +62,13 @@ export const getRecentPosts = async () => {
 
 export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
-    query GetPostDetails(${slug}: String!, ${categories}: [String!]) {
+    query getSimilarPosts($slug: String!, $categories: [String!]) {
       posts(
         where: {
-          slug_not: ${slug}
-          AND: { categories_some: { slug_in: ${categories} } }
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
         }
-        orderBy: createAt_ASC
+        orderBy: createdAt_ASC
         last: 3
       ) {
         title
@@ -81,9 +81,44 @@ export const getSimilarPosts = async (categories, slug) => {
     }
   `
 
-  const results = await request(graphqlAPI, query)
+  const results = await request(graphqlAPI, query, { slug, categories })
 
   return results.posts
+}
+
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!) {
+      post(where: { slug: $slug }) {
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        author {
+          bio
+          name
+          id
+          photo {
+            url
+          }
+        }
+        createdAt
+        slug
+        content {
+          raw
+        }
+        categories {
+          name
+          slug
+        }
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query, { slug })
+
+  return result.post
 }
 
 export const getCategories = async () => {
