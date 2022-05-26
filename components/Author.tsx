615109c5dc69c6ceb8f46597baa20c0177255e7a
josephhlwang <joseph001126@gmail.com>
2022-05-26 19:59:20 -0400

    Added Author component under posts.

--- components/Author.tsx
+++ components/Author.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 
-const Author = (author: any) => {
-  return <div>Author</div>
+const Author = ({ author }: any) => {
+  return (
+    <div className="relative mt-20 mb-8 rounded-lg bg-black bg-opacity-20 p-11 text-center">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image
+          unoptimized
+          src={author.photo.url}
+          alt={author.name}
+          height="100px"
+          width="100px"
+          className="rounded-full align-middle"
+        />
+      </div>
+      <h3 className="my-4 text-xl font-bold text-white">{author.name}</h3>
+      <p className="text-lg text-white">{author.bio}</p>
+    </div>
+  )
 }
 
 export default Author
