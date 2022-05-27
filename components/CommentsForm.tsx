e5ad5411a016610043ea0091472a05370cfe5879
josephhlwang <joseph001126@gmail.com>
2022-05-27 15:01:29 -0400

    Created comments form component.

--- components/CommentsForm.tsx
+++ components/CommentsForm.tsx
@@ -1,7 +1,74 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 const CommentsForm = (slug: any) => {
-  return <div>CommentsForm</div>
+  const [error, setError] = useState(false)
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+  const commentEl = useRef<any>(null)
+  const nameEl = useRef<any>(null)
+  const emailEl = useRef<any>(null)
+
+  const handlePostSubmission = () => {
+    setError(false)
+    const { value: name } = nameEl.current
+    const { value: comment } = commentEl.current
+    const { value: email } = emailEl.current
+
+    if (!name || !comment || email) {
+      setError(true)
+      return
+    }
+
+    const commentObj = { name, email, comment, slug }
+  }
+
+  return (
+    <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        Leave a Reply
+      </h3>
+      <div className="mb-4 grid grid-cols-1 gap-4">
+        <textarea
+          ref={commentEl}
+          className="w-full rounded-lg bg-gray-100 p-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+          name="comment"
+          placeholder="Comment"
+        />
+      </div>
+      <div className="mb-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
+        <input
+          ref={nameEl}
+          type="text"
+          className="w-full rounded-lg bg-gray-100 py-2 px-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          ref={emailEl}
+          type="email"
+          className="w-full rounded-lg bg-gray-100 py-2 px-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+      {error && (
+        <p className="text-xs text-red-500">All fields are mandatory</p>
+      )}
+      <div className="mt-8">
+        <button
+          type="button"
+          onClick={handlePostSubmission}
+          className="ease inline-block cursor-pointer rounded-full bg-pink-600 px-8 py-3 text-lg font-medium text-white transition duration-500 hover:bg-indigo-900"
+        >
+          Post Comment
+        </button>
+        {showSuccessMessage && (
+          <span className="float-right mt-3 text-xl font-semibold text-green-500">
+            Comment submitted for review
+          </span>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default CommentsForm
