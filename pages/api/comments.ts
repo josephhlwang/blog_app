6fc7803b3f4daa691741992f7e49aadf15d2e8b3
josephhlwang <joseph001126@gmail.com>
2022-05-28 00:40:30 -0400

    Added submit comment backend.

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -1,1 +1,45 @@
-import { gql, GraphQLClient } from "graphql-request";
+import { gql, GraphQLClient } from 'graphql-request'
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT!
+
+export default async function comments(req: any, res: any) {
+  const { name, email, comment, slug } = req.body
+
+  const graphQLClient = new GraphQLClient(graphqlAPI, {
+    headers: {
+      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+    },
+  })
+
+  const query = gql`
+    mutation CreateComment(
+      $name: String!
+      $email: String!
+      $comment: String!
+      $slug: String!
+    ) {
+      createComment(
+        data: {
+          name: $name
+          email: $email
+          comment: $comment
+          post: { connect: { slug: $slug } }
+        }
+      ) {
+        id
+      }
+    }
+  `
+  try {
+    const result = await graphQLClient.request(query, {
+      name,
+      email,
+      comment,
+      slug,
+    })
+    return res.status(200).send(result)
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send(err)
+  }
+}
--- services/index.js
+++ services/index.js
@@ -139,8 +139,12 @@ export const getCategories = async () => {
 }
 
 export const submitComment = async (obj) => {
+  console.log(obj)
   const result = await fetch('/api/comments', {
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify(obj),
   })
 
