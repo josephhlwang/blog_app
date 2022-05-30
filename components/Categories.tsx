9ccf4011f5ae35f3096cc0d1f14204e7a131e0c5
josephhlwang <joseph001126@gmail.com>
2022-05-30 15:51:42 -0400

    Changed route for categories.

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -14,7 +14,7 @@ const Categories = () => {
     <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
       <h3 className="mb-8 border-b pb-4 text-xl font-semibold">Categories</h3>
       {categories.map((category: any) => (
-        <Link key={category.slug} href={`category/${category.slug}`}>
+        <Link key={category.slug} href={`/category/${category.slug}`}>
           <span className="mb-3 block cursor-pointer pb-3">
             {category.name}
           </span>
