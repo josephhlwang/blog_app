fe89e9ff8417a516e895a467b338f4457686a5ca
josephhlwang <joseph001126@gmail.com>
2022-05-31 23:05:03 -0400

    Added responsive CSS to header.

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,14 +13,14 @@ const Header = () => {
   return (
     <div className="container mx-auto mb-8 px-10">
       <div className="inline-block w-full border-b border-blue-400 py-8">
-        <div className="block md:float-left">
+        <div className="block text-center md:float-left">
           <Link href="/">
             <span className="cursor-pointer text-4xl font-bold text-white">
-              GraphCMS
+              Joseph's Blog
             </span>
           </Link>
         </div>
-        <div className="hidden md:float-left md:contents">
+        <div className="text-md flex justify-center align-middle md:float-right md:text-xl">
           {categories.map((category: any) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
               <span className="mt-2 ml-4 cursor-pointer align-middle font-semibold text-white md:float-right">
