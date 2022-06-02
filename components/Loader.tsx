0d7ed484deb8853c9550ddf2c839886f47f53ae3
josephhlwang <joseph001126@gmail.com>
2022-06-02 19:10:40 -0400

    Changed loader color.

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -4,7 +4,7 @@ const Loader = () => (
   <div className="text-center">
     <button
       type="button"
-      className="inline-flex cursor-not-allowed items-center rounded-md border border-transparent bg-rose-600 px-4 py-2 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-rose-500 focus:border-rose-700 active:bg-rose-700"
+      className="inline-flex cursor-not-allowed items-center rounded-md border border-transparent bg-blue-500 px-4 py-2 text-base font-medium leading-6 text-white"
       disabled
     >
       <svg
